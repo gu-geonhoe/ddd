326cfd855b12461f136ccc5251e047bafe37de11
구건회 <rjsghl92@gmail.com>
2022-09-08 16:13:29 +0900

    a

---
+++ src/main/java/com/example/demo/MovieController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class MovieController {
+
+    @RequestMapping("/api/movie")
+    public List<String> MovieList()
+    {
+        return Arrays.asList("영화보기", "Movie");
+    }
+}
